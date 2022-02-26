85e6184f679720080a49d290eaf4114389c5a939
AJ Frank <ybafrank@gmail.com>
2022-02-26 09:52:25 -0500

    Added test cases for ReportStructure
    I covered 3 cases that made sense to me. This includes two edge cases (employee being queried is the root of the tree structure, or is a leaf node) as well as the average case (employee is a direct report to someone else, but also has direct reports).

--- code-challenge.Tests/ReportingStructureControllerTests.cs
+++ code-challenge.Tests/ReportingStructureControllerTests.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Net;
 using System.Net.Http;
-using System.Text;
+using System.Threading.Tasks;
+using challenge.Models;
+using code_challenge.Tests.Integration.Extensions;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace code_challenge.Tests.Integration
-{
+namespace code_challenge.Tests.Integration {
     [TestClass]
     public class ReportingStructureControllerTests {
         private static HttpClient httpClient;
@@ -28,13 +28,76 @@ namespace code_challenge.Tests.Integration
             testServer.Dispose();
         }
 
-        public ReportingStructureControllerTests() {
+        /// <summary>
+        /// Test that covers the case where the employee to query the report
+        /// structure for is the root of the tree structure.
+        /// </summary>
+        [TestMethod]
+        public void GetReportStructure_Returns_Expected_Value_John_Lennon() {
+            // Setup - expected direct reports and John Lennon EmployeeId
+            const int expected = 4;
+            const string employeeId = "16a596ae-edd3-4847-99fe-c4518e82c86f";
 
+            // Act
+            Task<HttpResponseMessage> getRequest = httpClient.GetAsync($"api/reporting/{employeeId}");
+
+            HttpResponseMessage response = getRequest.Result;
+
+            // Assert - verify we got status code 200 (expected)
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            ReportingStructure report = response.DeserializeContent<ReportingStructure>();
+            int actual = report.NumberOfReports;
+            // verify our actual report count matches the expected value.
+            Assert.AreEqual(expected, actual);
         }
 
+        /// <summary>
+        /// Test that covers the average case. The employee to query the report
+        /// structure for reports to an employee above them, but also has direct
+        /// reports of their own.
+        /// </summary>
         [TestMethod]
-        public void GetReportStructure_Returns_Expected_Value() {
+        public void GetReportStructure_Returns_Expected_Value_Ringo_Starr()
+        {
+            // Setup - expected direct reports and Ringo Starr EmployeeId
+            const int expected = 2;
+            const string employeeId = "03aa1462-ffa9-4978-901b-7c001562cf6f";
 
+            // Act
+            Task<HttpResponseMessage> getRequest = httpClient.GetAsync($"api/reporting/{employeeId}");
+
+            HttpResponseMessage response = getRequest.Result;
+
+            // Assert - verify we got status code 200 (expected)
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            ReportingStructure report = response.DeserializeContent<ReportingStructure>();
+            int actual = report.NumberOfReports;
+            // verify our actual report count matches the expected value.
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Test that covers the case where the employee to query the report
+        /// structure for has no direct reports. (This employee would be considered
+        /// a leaf node in the tree structure)
+        /// </summary>
+        [TestMethod]
+        public void GetReportStructure_Returns_Expected_Value_Paul_McCartney()
+        {
+            // Setup - expected direct reports and Paul McCartney EmployeeId
+            const int expected = 0;
+            const string employeeId = "b7839309-3348-463b-a7e3-5de1c168beb3";
+
+            Task<HttpResponseMessage> getRequest = httpClient.GetAsync($"api/reporting/{employeeId}");
+
+            HttpResponseMessage response = getRequest.Result;
+
+            // Assert - verify we got status code 200 (expected)
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            ReportingStructure report = response.DeserializeContent<ReportingStructure>();
+            int actual = report.NumberOfReports;
+            // verify our actual report count matches the expected value.
+            Assert.AreEqual(expected, actual);
         }
     }
 }
